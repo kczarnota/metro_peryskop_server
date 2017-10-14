80bd00ac51dad6c8c38315db5c641b4332802cef
Aleksander Brodowicz <aleksanderbrodowicz@gmail.com>
2017-10-14 13:12:35 +0200

    cleanup: removed front end and useless dependencies

--- src/main/webapp/app/components/form/pagination.constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('printingServiceApp')
-        .constant('paginationConstants', {
-            'itemsPerPage': 20
-        });
-})();
